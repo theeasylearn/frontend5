06e9634248325640df68068d75603971aac8552a
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2022-08-29 19:41:57 +0530

    basic

---
+++ javascript/lesson2.js
@@ -0,0 +1,6 @@
+// alert("javascript(JS) is working");
+console.log("this is log message");
+console.warn("this is warning message");
+console.error("this is error message");
+console.debug("this is debug message");
+console.info("this is info message");
