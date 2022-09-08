b8d7bd346deeef5b11d4136b9a64561984ec712e
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2022-09-08 20:11:22 +0530

    javascript

---
+++ javascript/mymath.js
@@ -0,0 +1,18 @@
+function add(num1,num2)
+{
+    var result = num1 + num2; //result is local variable
+    return result;
+}
+function sub(num1,num2)
+{
+    var result = num1 - num2; //result is local variable
+    return result;
+}
+function mul(a,b)
+{
+    return a * b;
+}
+function div(x,y)
+{
+    return x/y;
+}
